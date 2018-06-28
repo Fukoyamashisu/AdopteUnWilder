704e0bcfe9ab5aef2fecb230537aebdf14e42c7f
Fafanback <barre.sylv@gmail.com>
2018-06-28 19:40:41 +0200

    creation CardStack

--- src/components/CardStack.js
+++ src/components/CardStack.js
@@ -1,24 +1,40 @@
 import React, {Component} from 'react';
-import {StyledCardStack} from '../styled/StyledCardStack'
-import {Col, Row} from 'reactstrap'
+import {StyledCardStack, StyledH2, StyledP, StyledContainer} from '../styled/StyledCardStack'
+import {Col, Row, Container} from 'reactstrap'
 class CardStack extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            stack: ["React", "JavaScript", "Angular", "Vue"]
+            stack: [
+                "React",
+                "JavaScript",
+                "Angular",
+                "Vue",
+                "Pug",
+                "PHP",
+                "$?$?$?",
+                "MVC",
+                "Symfo",
+                "Fortran",
+                "Haskell",
+                "Lodash",
+                "Passport",
+                "BCrypt"
+            ]
         };
     }
     render() {
+        const {stack} = this.state;
         return (
             <StyledCardStack>
-                <Row>
-                    <Col lg="5">
-                        <p>React</p>
-                    </Col>
-                    <Col lg="5">
-                        <p>JavaScript</p>
-                    </Col>
-                </Row>
+                <StyledH2>Stacks Techniques</StyledH2>
+                <StyledContainer>
+                    <Row justify-content="center">
+                        {stack.map((elt, i) => <Col md="4" key={i}>
+                            <StyledP>{elt}</StyledP>
+                        </Col>)}
+                    </Row>
+                </StyledContainer>
             </StyledCardStack>
         );
     }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import theme from './styled/theme';
 import {ThemeProvider} from 'styled-components';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter as Router} from 'react-router-dom';
-
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {fab} from '@fortawesome/free-brands-svg-icons'
 import {faCheckSquare, faCoffee} from '@fortawesome/free-solid-svg-icons'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 library.add(fab, faCheckSquare, faCoffee)
 
