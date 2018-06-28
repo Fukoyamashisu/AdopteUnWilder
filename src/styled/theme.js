022f6268440d3c9a64441ad6a09e84b28f8a243a
Fukoyamashisu <anthony.troupenat@gmail.com>
2018-06-28 14:41:09 +0200

    add theme

---
+++ src/styled/theme.js
@@ -0,0 +1,11 @@
+
+
+const theme = {
+    orange: "#fa7a0f",
+    red: "#b64426",
+    blue: "#5eb1bf",
+    lightBlue: "#dce9ef",
+    darkBlue: "#006e90",
+    black:"#252526",
+    grey:"#fcfcfc"
+}
