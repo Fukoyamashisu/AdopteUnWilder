86581f6a820425a1d932f2a8ccf7bb83284ffa9c
Fafanback <barre.sylv@gmail.com>
2018-06-29 00:19:06 +0200

    creation CardProject

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import Routes from '../routes.js';
+import CardProject from './CardProject'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Routes {...this.props}/>
+        <CardProject/>
       </div>
     );
   }
---
+++ src/styled/StyledCardProject.js
@@ -0,0 +1,40 @@
+import Styled from 'styled-components';
+import {Link} from 'react-router-dom'
+
+export const StyledCardProject = Styled.div `
+    width: 100%;
+    margin-top: 6%;
+    font-family: Cardo;
+    font-size: 17px;
+    background: ${props => props.theme.grey};
+    box-shadow: 3px 3px 10px ${props => props.theme.blue};
+    border-radius: 3px;
+`;
+
+export const StyledFooter = Styled.div `
+    min-height: 100px;
+    width: 100%;
+    margin-top: 3%;
+    font-family: Lato;
+    background: ${props => props.theme.grey};
+`;
+export const StyledH2 = Styled.h2 `
+        padding-top: 4%;
+        margin-left: 4%;
+
+`;
+export const StyledP = Styled.p `
+        margin-left: 4%;
+`;
+export const StyledA = Styled.a `
+        color: ${props => props.theme.orange};
+        margin-left: 4%;
+`;
+export const StyledCardHeader = Styled.div `
+    height:250px;
+    width: 100%;
+    background-image: url(https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png);
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`
