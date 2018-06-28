9305799630b69876d17815efdab4aa0f66ffac20
Julien Foucher <blomki@gmail.com>
2018-06-28 22:24:34 +0200

    reprise du menu left

--- src/faac/SideMenu.js
+++ src/faac/SideMenu.js
@@ -1,12 +1,22 @@
 //import { StyledSideMenu } from "../styled/StyledSideMenu";
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
-import { MainHomeTitle, MainHomeDescription } from "../styled/LeftMenu";
+import { Row, Col } from "reactstrap";
+import {
+  MainHomeTitle,
+  MainHomeDescription,
+  Divider,
+  NavItem
+} from "../styled/LeftMenu";
 
 export const SideMenu = ({ children, match }) => {
   return (
     <Row>
-      <Col xs="3">menu gauche</Col>
+      <Col xs="3">
+        <ul style={{}}>
+          <NavItem>Portfolio</NavItem>
+          <NavItem>Wild Code School</NavItem>
+        </ul>
+      </Col>
       <Col xs="9">
         <img
           src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
@@ -16,6 +26,7 @@ export const SideMenu = ({ children, match }) => {
         <MainHomeTitle color="black">New Wilders</MainHomeTitle>
         <MainHomeDescription>
           No matter what people tell you, words and ideas can change the world.
+          <Divider />
         </MainHomeDescription>
         <p>Filtre 1</p>
         <p>Filtre 2</p>
--- src/styled/LeftMenu.js
+++ src/styled/LeftMenu.js
@@ -4,6 +4,7 @@ export const MainHomeTitle = Styled.h1`
 font-family: "Lato";
 font-size: 4rem;
 font-weight:900;
+color: ${props => props.theme.red};
 `;
 
 export const MainHomeDescription = Styled.p`
@@ -11,4 +12,18 @@ font-family: "Cardo";
 font-size: 1.3rem;
 line-Height:1.5rem;
 font-style: italic;
+`;
+
+export const Divider = Styled.span`
+margin:30px 0px;
+width: 50%;
+border-bottom: 2px solid ${props => props.theme.black};
+`;
+
+export const NavItem = Styled.li`
+    writing-mode: vertical-rl;
+    text-orientation: sideways;
+    list-style-type: none;
+    margin: 3rem 0rem 3rem 0rem;
+    text-align: center;
 `;
