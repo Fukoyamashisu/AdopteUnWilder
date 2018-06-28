097b58f81e1a3928bc7013dd4f9694be1da687c7
romain coquery <coquery.romain@gmail.com>
2018-06-28 15:28:26 +0200

    Routing hosts

--- back/src/AppBundle/Controller/Api/DefaultController.php
+++ back/src/AppBundle/Controller/Api/DefaultController.php
@@ -9,11 +9,11 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="api.home", host="api.{domain}", defaults={"domain"="%domain%"})
+     * @Route("/", name="api.home")
      */
     public function indexAction(Request $request)
     {
-        die('f');
+        die('api');
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
