03038452674fcd7142e1eadc14a45b28bfd89ab1
Julien Foucher <blomki@gmail.com>
2018-06-29 14:05:09 +0200

    modif sur profile

--- src/components/CardStack.js
+++ src/components/CardStack.js
@@ -1,35 +1,42 @@
-import React, {Component} from 'react';
-import {StyledCardStack, StyledH2, StyledP, StyledContainer} from '../styled/StyledCardStack'
-import {Col, Row, Container} from 'reactstrap'
+import React, { Component } from "react";
+import {
+  StyledCardStack,
+  StyledH2,
+  StyledP,
+  StyledContainer
+} from "../styled/StyledCardStack";
+import { Col, Row, Container } from "reactstrap";
 class CardStack extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            orderedSkills: []
-        };
-    }
-    componentDidMount() {
-        const {stacks} = this.props;
-        const orderedSkills = stacks.sort((a, b) => a.order - b.order)
-        return this.setState({orderedSkills})
-    }
-    render() {
-        const {orderedSkills} = this.state;
-        return (
-            <Row>
-                <StyledCardStack md="10">
-                    <StyledH2>Stacks Techniques</StyledH2>
-                    <StyledContainer>
-                        <Row justify-content="center">
-                            {orderedSkills.map((elt, i) => <Col key={i} md="4">
-                                <StyledP>{elt.name}</StyledP>
-                            </Col>)}
-                        </Row>
-                    </StyledContainer>
-                </StyledCardStack>
+  constructor(props) {
+    super(props);
+    this.state = {
+      orderedSkills: []
+    };
+  }
+  componentDidMount() {
+    const { stacks } = this.props;
+    const orderedSkills = stacks.sort((a, b) => a.order - b.order);
+    return this.setState({ orderedSkills });
+  }
+  render() {
+    const { orderedSkills } = this.state;
+    return (
+      <Row>
+        <StyledCardStack md="10">
+          <StyledH2>Stack Technique</StyledH2>
+          <StyledContainer>
+            <Row justify-content="center">
+              {orderedSkills.map((elt, i) => (
+                <Col key={i} md="4">
+                  <StyledP>{elt.name}</StyledP>
+                </Col>
+              ))}
             </Row>
-        );
-    }
+          </StyledContainer>
+        </StyledCardStack>
+      </Row>
+    );
+  }
 }
 
 export default CardStack;
--- src/styled/StyledCardProject.js
+++ src/styled/StyledCardProject.js
@@ -1,39 +1,39 @@
-import Styled from 'styled-components';
-import {Link} from 'react-router-dom'
+import Styled from "styled-components";
+import { Link } from "react-router-dom";
 
-export const StyledCardProject = Styled.div `
+export const StyledCardProject = Styled.div`
     width: 100%;
-    margin-top: 6%;
-    font-family: Cardo;
+    margin-top: 30px;
     font-size: 17px;
     background: ${props => props.theme.grey};
-    box-shadow: 3px 3px 10px ${props => props.theme.blue};
+    box-shadow: 3px 3px 10px ${props => props.theme.dark};
     border-radius: 3px;
 `;
 
-export const StyledFooter = Styled.div `
+export const StyledFooter = Styled.div`
     min-height: 100px;
     width: 100%;
     margin-top: 3%;
     font-family: Lato;
     background: ${props => props.theme.grey};
 `;
-export const StyledH2 = Styled.h2 `
+export const StyledH2 = Styled.h2`
         padding-top: 4%;
         margin-left: 4%;
+        font-family: Lato;
 `;
-export const StyledP = Styled.p `
-        margin-left: 4%;
+export const StyledP = Styled.p`
+        margin-left: 0;
 `;
-export const StyledA = Styled.a `
+export const StyledA = Styled.a`
         color: ${props => props.theme.orange};
         margin-left: 4%;
 `;
-export const StyledCardHeader = Styled.div `
+export const StyledCardHeader = Styled.div`
     height:250px;
     width: 100%;
     background-image: url(https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png);
     background-position: center center;
     background-repeat: no-repeat;
     background-size: cover;
-`
+`;
