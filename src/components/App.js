b524be7e6d9a98cde1a62ea16f04010f78c87b6a
Fafanback <barre.sylv@gmail.com>
2018-06-29 10:31:42 +0200

    corrections

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import Routes from '../routes.js';
-import {InfoWild} from '../faac/InfoWild'
+
 class App extends Component {
   render() {
-    //<Routes {...this.props}/>
     return (
       <div>
-        <InfoWild/>
+        <Routes {...this.props}/>
       </div>
 
     );
