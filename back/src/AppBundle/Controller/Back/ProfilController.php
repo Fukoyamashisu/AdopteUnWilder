5dd5d1b2d29d58a82aa895eb76f2a4f9b1d0fb4c
Aurélien Roche <roche426@gmail.com>
2018-06-29 11:20:24 +0200

    create profil

--- back/src/AppBundle/Controller/Back/ProfilController.php
+++ back/src/AppBundle/Controller/Back/ProfilController.php
@@ -3,11 +3,15 @@
 namespace AppBundle\Controller\Back;
 
 use AppBundle\Entity\Profil;
+use AppBundle\Entity\User;
 use AppBundle\Service\ImageManipulator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProfilController extends Controller
 {
@@ -16,41 +20,53 @@ class ProfilController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $em = $this->getDoctrine()->getRepository(Profil::class);
-        $user = $em->find( 1);
+        $em = $this->getDoctrine()->getManager();
+        $profil = $em->getRepository(Profil::class)->findOneBy([
+            'user' => $this->getUser()
+        ]);
 
-        return $this->render('profil/index.html.twig', ['user' => $user]);
+        if ($profil) {
+
+            return $this->render('profil/index.html.twig', [
+                'user' => $profil,
+            ]);
+        } else {
+
+            return $this->redirectToRoute('back.user.new');
+        }
     }
 
     /**
-     * @Route("/user-edit", name="back.user.edit")
+     * @Route("/user-edit/{profil_id}", name="back.user.edit", requirements={"profil_id"="\d+"})
+     * @Method({"GET", "POST"})
+     * @ParamConverter("profil", options={"id" = "profil_id"})
      */
-    public function editAction(Request $request, ImageManipulator $imageManipulator)
+    public function editAction(Request $request, ImageManipulator $imageManipulator, Profil $profil)
     {
-        $user = $this->getUser()->get;
+        $user = $this->getUser();
 
-        $currentProfilPicture = $user->getProfilPicture();
-        $currentCoverPicture = $user->getCoverPicture();
-        $currentCvLink = $user->getCvLink();
+        $currentProfilPicture = $profil->getProfilPicture();
+        $currentCoverPicture = $profil->getCoverPicture();
+        $currentCvLink = $profil->getCvLink();
 
-        $editForm = $this->createForm('AppBundle\Form\ProfilType', $user);
+        $editForm = $this->createForm('AppBundle\Form\ProfilType', $profil);
         $editForm->handleRequest($request);
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
 
             if ($editForm['profilPicture']->getdata() === null ) {
-                $user->setProfilPicture($currentProfilPicture);
+                $profil->setProfilPicture($currentProfilPicture);
             }
 
             if ($editForm['profilPicture']->getdata() ){
 
-            $pictureProfil = $user->getProfilPicture();
+            $pictureProfil = $profil->getProfilPicture();
 
             $fileNamePicture = uniqid() . '.' . $pictureProfil->guessExtension();
 
             $imageManipulator->handleUploadedProfilPicture($pictureProfil, $fileNamePicture);
 
-            $user->setProfilPicture('assets/images/uploaded/profilPicture/' . $fileNamePicture);
+                $profil->setProfilPicture('assets/images/uploaded/profilPicture/' . $fileNamePicture);
 
             if ($currentProfilPicture ) {
                 unlink($currentProfilPicture);
@@ -58,7 +74,7 @@ class ProfilController extends Controller
             }
 
             if ($editForm['coverPicture']->getdata() === null ) {
-                $user->setProfilPicture($currentCoverPicture);
+                $profil->setProfilPicture($currentCoverPicture);
             }
 
             if ($editForm['coverPicture']->getdata()) {
@@ -66,7 +82,7 @@ class ProfilController extends Controller
             $pictureCover = $user->getCoverPicture();
             $fileNameCover = uniqid() . '.' . $pictureCover->guessExtension();
             $imageManipulator->handleUploadedCoverPicture($pictureCover, $fileNameCover);
-            $user->setCoverPicture('assets/images/uploaded/coverPicture/' . $fileNameCover);
+                $profil->setCoverPicture('assets/images/uploaded/coverPicture/' . $fileNameCover);
 
                 if($currentCoverPicture) {
                     unlink($currentCoverPicture);
@@ -74,14 +90,14 @@ class ProfilController extends Controller
             }
 
             if ($editForm['cvLink']->getdata() === null ) {
-                    $user->setCvLink($currentCvLink);
+                $profil->setCvLink($currentCvLink);
             }
 
             if ($editForm['cvLink']->getdata() ){
 
                 $cvLink = $user->getCvLink();
                 $fileNameCv = uniqid() . '.' . $cvLink->guessExtension();
-                $user->setCvLink('assets/images/uploaded/profilPicture/' . $fileNameCv);
+                $profil->setCvLink('assets/images/uploaded/profilPicture/' . $fileNameCv);
 
                 if ($currentCvLink ) {
                     unlink($currentCvLink);
@@ -90,7 +106,7 @@ class ProfilController extends Controller
 
 
         $em = $this->getDoctrine()->getManager();
-        $em->persist($user);
+        $em->persist($profil);
         $em->flush();
 
 
@@ -99,6 +115,72 @@ class ProfilController extends Controller
 
         return $this->render('profil\edit.html.twig', array(
             'edit_form' => $editForm->createView(),
+        ));
+
+    }
+
+
+    /**
+     * @Route("/user-new", name="back.user.new")
+     * @Method({"GET", "POST"})
+     */
+    public function addProfilAction(Request $request, ImageManipulator $imageManipulator)
+    {
+
+        $profil = new Profil();
+
+        $newProfilForm = $this->createForm('AppBundle\Form\ProfilType', $profil);
+        $newProfilForm->handleRequest($request);
+
+        if ($newProfilForm->isSubmitted() && $newProfilForm->isValid()) {
+
+
+            if ($newProfilForm['profilPicture']->getdata() ){
+
+                $pictureProfil = $profil->getProfilPicture();
+
+                $fileNamePicture = uniqid() . '.' . $pictureProfil->guessExtension();
+
+                $imageManipulator->handleUploadedProfilPicture($pictureProfil, $fileNamePicture);
+
+                $profil->setProfilPicture('assets/images/uploaded/profilPicture/' . $fileNamePicture);
+
+            }
+
+
+            if ($newProfilForm['coverPicture']->getdata()) {
+
+                $pictureCover = $profil->getCoverPicture();
+                $fileNameCover = uniqid() . '.' . $pictureCover->guessExtension();
+                $imageManipulator->handleUploadedCoverPicture($pictureCover, $fileNameCover);
+                $profil->setCoverPicture('assets/images/uploaded/coverPicture/' . $fileNameCover);
+
+            }
+
+
+//            if ($newProfilForm['cvLink']->getdata() ){
+//
+//                $cvLink = $profil->getCvLink();
+//                $fileNameCv = uniqid() . '.' . $cvLink->guessExtension();
+//                $profil->setCvLink('assets/images/uploaded/profilPicture/' . $fileNameCv);
+//
+//                if ($currentCvLink ) {
+//                    unlink($currentCvLink);
+//                }
+//            }
+
+            $profil->setUser($this->getUser());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($profil);
+            $em->flush();
+
+
+            return $this->redirectToRoute('back.user.index');
+        }
+
+        return $this->render('profil\edit.html.twig', array(
+            'edit_form' => $newProfilForm->createView(),
         ));
 
     }
