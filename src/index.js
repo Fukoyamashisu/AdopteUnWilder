3cbcc3c11745efed6f8b5faad1148a460085d182
Julien Foucher <blomki@gmail.com>
2018-06-29 10:27:36 +0200

    ajout de sideMenu sur la home

--- src/index.js
+++ src/index.js
@@ -6,28 +6,26 @@ import theme from "./styled/theme";
 import { ThemeProvider } from "styled-components";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faSearch,
+  faChevronRight,
+  faChevronLeft
+} from "@fortawesome/free-solid-svg-icons";
 
+library.add(faChevronRight, faChevronLeft, faBars, faSearch);
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-<<<<<<< HEAD
-import { faChevronRight, faChevronLeft,faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
-=======
-import { faCheckSquare, faCoffee, faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
->>>>>>> 698257766fbd1ce991253dffe1f9f93ea74c1d15
-
-
-library.add( faChevronRight, faChevronLeft, faBars, faSearch);
-
-if (module.hot) 
-    module.hot.accept();
+if (module.hot) module.hot.accept();
 
 ReactDOM.render(
-    <Router>
+  <Router>
     <ThemeProvider theme={theme}>
-        <App/>
+      <App />
     </ThemeProvider>
-</Router>, document.getElementById('root'));
+  </Router>,
+  document.getElementById("root")
+);
 
 registerServiceWorker();
